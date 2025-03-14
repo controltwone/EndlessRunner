6b87bc175bf8834d0168f59c6c66b860d0cce427
Ferhat Oguncer <96962387+controltwone@users.noreply.github.com>
2025-03-14 20:46:15 +0300

    v1.0 player skin and some animations added via Mixamo.

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float moveSpeed = 3;
+    public float leftrightSpeed = 4;
+    public  float rightLimit = 5f;
+    public  float leftLimit = -5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed * Time.deltaTime * Vector3.forward, Space.World);
+        //forward , z ekseninde pozitif gitmektir.
+        //backward, z ekseninde negatif yönde gitmektir.
+        if(Input.GetAxisRaw("Horizontal") < 0) // GetAxisRaw -1, 0, ve 1 döndürür.
+        {
+            if(this.gameObject.transform.position.x > leftLimit ) //platformdan disari cikmasin diye sinirlarimizi da ayarladik.
+            {
+            transform.Translate(Vector3.left * Time.deltaTime * leftrightSpeed  );
+            }
+            
+        }
+         if(Input.GetAxisRaw("Horizontal") > 0) // GetAxisRaw -1, 0, ve 1 döndürür.
+        {
+            if(this.gameObject.transform.position.x < rightLimit ) //platformdan disari cikmasin diye sinirlarimizi da ayarladik.
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * leftrightSpeed);
+            }
+        }
+        
+    }
+}
