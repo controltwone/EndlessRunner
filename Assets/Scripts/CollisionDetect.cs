c47d861389cb51ad38c3ffe9c07ecc92196f9804
Ferhat Oguncer <96962387+controltwone@users.noreply.github.com>
2025-03-18 01:21:44 +0300

    v3.1 Falling and camera animation were added.

--- Assets/Scripts/CollisionDetect.cs
+++ Assets/Scripts/CollisionDetect.cs
@@ -1,12 +1,26 @@
+using System.Collections;
 using UnityEngine;
 
 public class CollisionDetect : MonoBehaviour
 {
     [SerializeField] GameObject player;
     [SerializeField] GameObject playerAnim;
+    [SerializeField] AudioSource collisionFX;
+    [SerializeField] GameObject mainCam;
+    [SerializeField] GameObject fadeOut;
     void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(CollisionEnd());
+    }
+
+    IEnumerator CollisionEnd()
     {
         player.GetComponent<PlayerMove>().enabled = false;
         playerAnim.GetComponent<Animator>().Play("Stumble Backwards");
+        collisionFX.Play();
+        mainCam.GetComponent<Animator>().Play("CameraFalling");
+        yield return new WaitForSeconds(3);
+        fadeOut.SetActive(true);
+
     }
 }
